9c5d3fe6e7fd874a2316f64175ab7aad35b4d374
Evgeniy <eugenej.job@gmail.com>
2016-05-23 21:46:14 +0300

    version 0.1.5 added materials load system

--- src/ua/edu/cdu/pm3/ChobotarEV/managment/fileLoaders/MTLLoader.java
+++ src/ua/edu/cdu/pm3/ChobotarEV/managment/fileLoaders/MTLLoader.java
@@ -1,65 +1,70 @@
 package ua.edu.cdu.pm3.ChobotarEV.managment.fileLoaders;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
-import org.lwjgl.util.vector.Vector3f;
+import org.newdawn.slick.opengl.Texture;
 import ua.edu.cdu.pm3.ChobotarEV.rendering.renderComponents.Material;
+import ua.edu.cdu.pm3.ChobotarEV.rendering.renderComponents.Model;
 
 public class MTLLoader {
 
-    public static Material loadMTL(String path) {
+    public static void parseMTLfile(Model model,File file) {
         try {
-            
-            BufferedReader reader = new BufferedReader(new FileReader("res/models/"+path));
-            String line;
-            Material material = new Material();
-            String  oName;
-            String  texPath;
+            BufferedReader reader = new BufferedReader(new FileReader("res/models/"+file));
+            String  line;
+            String name = "";
+            float ns = 0;
+            float d = 0;
+            float[]  ka = null;
+            float[]  kd = null;
+            float[]  ks = null;
+            Texture tex = null;
             while((line = reader.readLine()) != null) {
                 if(line.startsWith("newmtl ") ) {
-                    oName = line.split(" ")[1];
-                    material.addMaterialName(oName);
-                }
+                      name = line.split(" ")[1];
+                }else
+                    
                 if(line.startsWith("Ns ")) {
-                    material.addMaterialNs(Float.valueOf(line.split(" ")[1]));
-                }
+                    ns = Float.valueOf(line.split(" ")[1]);
+                }else
+                    
                 if(line.startsWith("Ka ")) {
-                    Vector3f ka = new Vector3f(Float.valueOf(line.split(" ")[1]),
-                                               Float.valueOf(line.split(" ")[2]),
-                                               Float.valueOf(line.split(" ")[3]));
-                    material.addMaterialKa(ka);
-                }
+                    ka = new float[4];
+                    ka[0] = Float.valueOf(line.split(" ")[1]);
+                    ka[1] = Float.valueOf(line.split(" ")[2]);
+                    ka[2] = Float.valueOf(line.split(" ")[3]);
+                    ka[3] = 1;
+                }else
+                    
                 if(line.startsWith("Kd ")) {
-                    Vector3f kd = new Vector3f(Float.valueOf(line.split(" ")[1]),
-                                               Float.valueOf(line.split(" ")[2]),
-                                               Float.valueOf(line.split(" ")[3]));
-                    material.addMaterialKd(kd);
-                }
+                    kd = new float[4];
+                    kd[0] = Float.valueOf(line.split(" ")[1]);
+                    kd[1] = Float.valueOf(line.split(" ")[2]);
+                    kd[2] = Float.valueOf(line.split(" ")[3]);
+                    kd[3] = 1;
+                }else
+                    
                 if(line.startsWith("Ks ")) {
-                    Vector3f ks = new Vector3f(Float.valueOf(line.split(" ")[1]),
-                                               Float.valueOf(line.split(" ")[2]),
-                                               Float.valueOf(line.split(" ")[3]));
-                    material.addMaterialKs(ks);
-                }
-                if(line.startsWith("Ke ")) {
-                    Vector3f ke = new Vector3f(Float.valueOf(line.split(" ")[1]),
-                                               Float.valueOf(line.split(" ")[2]),
-                                               Float.valueOf(line.split(" ")[3]));
-                    material.addMaterialKe(ke);
-                }
+                    ks = new float[4];
+                    ks[0] = Float.valueOf(line.split(" ")[1]);
+                    ks[1] = Float.valueOf(line.split(" ")[2]);
+                    ks[2] = Float.valueOf(line.split(" ")[3]);
+                    ks[3] = 1;
+                }else
+                    
                 if(line.startsWith("d ")) {
-                    material.addMaterialD(Float.valueOf(line.split(" ")[1]));
-                }
-                if(line.startsWith("map_Bump ")) {
-                    texPath = line.split(" ")[1];
-                    material.addMaterialTexture(TexturesLoader.loadTextures(texPath));
+                    d = Float.valueOf(line.split(" ")[1]);
+                   model.materials.add(new Material(name, tex, ka, kd, ks, ns, d));
+                }else
+                    
+                if(line.startsWith("map_Kd ")) {
+//                    tex = TexturesLoader.loadTextures(line.split(" ")[1]);
+                    
                 }
                 
             }
-            return material;
-        } catch (Exception e) {
-            
-        }
-        return null;
+            reader.close();
+        } catch (Exception e) {}
     }
 }
--- src/ua/edu/cdu/pm3/ChobotarEV/rendering/renderComponents/Model.java
+++ src/ua/edu/cdu/pm3/ChobotarEV/rendering/renderComponents/Model.java
@@ -1,6 +1,7 @@
 package ua.edu.cdu.pm3.ChobotarEV.rendering.renderComponents;
 
 import java.io.File;
+import java.nio.FloatBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import static org.lwjgl.opengl.GL11.*;
@@ -15,43 +16,56 @@ public class Model {
     public List<Vector3f>   normals         = new ArrayList<>();
     public List<Vector2f>   textureUV       = new ArrayList<>();
     public List<Face>       faces           = new ArrayList<Face>();
-    public static int       objectList;
-    public static Model     model;
+    public static 
+            List<Material>   materials       = new ArrayList<Material>();
+    public float posX       =10;//60;
+    public float posY       =0;//30;
+    public float posZ       =10;//55;
+    public static int       modelList;
+    public static Model     house;
    
     
     public void initializeModels() {
-        model = OBJLoader.loadModel(new File("House1/house.obj"));
-//        MTLLoader.loadMTL("House1/house.mtl");
+        house = OBJLoader.parseOBJfile(new File("House1/house.obj"));
+        MTLLoader.parseMTLfile(house,new File("House1/house.mtl"));
+        drawModel(house);
     }
     
-    public void drawModel() {
-        glPolygonMode (GL_FRONT_AND_BACK, GL_LINE);
-        glColor3f(0.5f, 0, 0);
-        objectList = glGenLists(2);
-        glNewList(objectList, GL_COMPILE); 
-//        bindTexture(0);
-//        glColorMaterial ( GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE ) ;
-//        glEnable ( GL_COLOR_MATERIAL ) ;
-        int i=0;
+    public void drawModel(Model model) {
+//        glPolygonMode (GL_FRONT_AND_BACK, GL_LINE);
+//        glColor3f(0.7f, 0, 0);
+        modelList = glGenLists(2);
+       
+        glNewList(modelList, GL_COMPILE);
+        
+        glEnable(GL_LIGHTING);
+        glEnable(GL_LIGHT0);
+        glEnable(GL_AMBIENT);
+        glEnable(GL_DIFFUSE);
+        glEnable(GL_SPECULAR);
+        
+        int currentMaterial = 0;
+        Material material = model.materials.get(currentMaterial);
+        setMaterial(material.texture,material.ambientColor,material.diffuseColor,material.specularColor,material.specularExponent);
         for(Face face : model.faces) {
-//            if(!face.oName.equals(Material.materialName.get(i))) {
-//                i=findInd(face.oName);
-//                bindTexture(i);
-//            }
-
-
+            if(!face.oName.equals(material.materialName)) {
+                material = model.materials.get(findInd(model,face.oName));
+                setMaterial(material.texture,material.ambientColor,material.diffuseColor,material.specularColor,material.specularExponent);
+            }
+            
             glBegin(GL_QUADS);
+            
             Vector3f vertices1      = model.vertices.get((int)face.vertices.x - 1);
 //            Vector2f textureCoord1  = model.textureUV.get((int)face.textures.x - 1);
             Vector3f normal1        = model.normals.get((int)face.normals.x - 1);
-            glVertex3f(vertices1.x, vertices1.y, vertices1.z);
+            glVertex3f(vertices1.x+posX, vertices1.y+posY, vertices1.z+posZ);
             glNormal3f(normal1.x, normal1.y, normal1.z);
 //            glTexCoord2f(textureCoord1.x,textureCoord1.y);
 
             Vector3f vertices2      = model.vertices.get((int)face.vertices.y - 1);
 //            Vector2f textureCoord2  = model.textureUV.get((int)face.textures.y - 1);
             Vector3f normal2        = model.normals.get((int)face.normals.y - 1);
-            glVertex3f(vertices2.x, vertices2.y, vertices2.z);
+            glVertex3f(vertices2.x+posX, vertices2.y+posY, vertices2.z+posZ);
             glNormal3f(normal2.x, normal2.y, normal2.z);
 //            glTexCoord2f(textureCoord2.x,textureCoord2.y);
             
@@ -59,7 +73,7 @@ public class Model {
             Vector3f vertices3      = model.vertices.get((int)face.vertices.z - 1);
 //            Vector2f textureCoord3  = model.textureUV.get((int)face.textures.z - 1);
             Vector3f normal3        = model.normals.get((int)face.normals.z - 1);
-            glVertex3f(vertices3.x, vertices3.y, vertices3.z);
+            glVertex3f(vertices3.x+posX, vertices3.y+posY, vertices3.z+posZ);
             glNormal3f(normal3.x, normal3.y, normal3.z);
 //            glTexCoord2f(textureCoord3.x,textureCoord3.y);
 
@@ -67,11 +81,11 @@ public class Model {
                 Vector3f vertices4      = model.vertices.get((int)face.vertices.w - 1);
 //                Vector2f textureCoord4  = model.textureUV.get((int)face.textures.w - 1);
                 Vector3f normal4        = model.normals.get((int)face.normals.w - 1);
-                glVertex3f(vertices4.x, vertices4.y, vertices4.z);
+                glVertex3f(vertices4.x+posX, vertices4.y+posY, vertices4.z+posZ);
                 glNormal3f(normal4.x, normal4.y, normal4.z);
 //                glTexCoord2f(textureCoord4.x,textureCoord4.y);
             } catch (ArrayIndexOutOfBoundsException e) {}
-           
+            
             glEnd();
 
         }
@@ -79,17 +93,25 @@ public class Model {
         glEndList();
         
     }
-    public void bindTexture(int i) {
-        if(i>26) i = 0;
-        glBindTexture(GL_TEXTURE_2D,Material.texture.get(i).getTextureID());
+    
+    public void setMaterial(Texture texture,FloatBuffer ka,FloatBuffer kd,FloatBuffer ks,float ns) {
+        
+//        System.err.println(texture.getTextureID());
+//        glBindTexture(GL_TEXTURE_2D,texture.getTextureID());
+        glMaterial(GL_FRONT_AND_BACK, GL_AMBIENT, ka);
+        glMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE, kd);
+        glMaterial(GL_FRONT_AND_BACK, GL_SPECULAR, ks);
+//        glMaterial(GL_FRONT_AND_BACK, GL_EMISSION, 0f);
+        glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, ns); 
     }
     
-    public int findInd(String oN) {
-        int ind = 0;
-        for(int i=0;i<Material.materialName.size();i++) {
-            if(Material.materialName.get(i).equals(oN))
-                ind = i;
+    public int findInd(Model model,String oN) {
+        int ind = 1;
+        for(Material material : model.materials) {
+            if(material.materialName.equals(oN))
+                return ind;
+            ind++;
         }
-        return ind;
+        return 0;
     }
 }
